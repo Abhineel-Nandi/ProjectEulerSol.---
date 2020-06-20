d71d0cf95f6608b5668fb1242160b067dbe3dfa1
Abhineel-Nandi <noobNandi04@gmail.com>
2020-06-20 13:12:06 +0530

    Create 31.cpp

---
+++ 31.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int coins[8] = {200, 100, 50, 20, 10, 5,2,1};
+
+int findposs(int money, int maxcoin)
+{
+    int sum = 0;
+    if(maxcoin == 7) return 1;
+    for(int i = maxcoin; i<8;i++)
+    {
+        if (money-coins[i] == 0) sum+=1;
+        if (money-coins[i] > 0) sum+=findposs(money-coins[i], i);
+    }
+    return sum;     
+}
+
+int main()
+{
+    cout<<findposs(200, 0)<<endl;
+}
